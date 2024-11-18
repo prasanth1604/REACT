45ec8ce9dcf07f3e480856ada411aa9bbd75a4c7
prasanth1604 <prasanthpadavala1@gmail.com>
2024-11-18 22:25:40 +0530

    footer fix

---
+++ portfolio-v2/src/Footer.jsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+function Footer(){
+    return(
+        <div className="footer">
+            <p>Copyright 2020</p>
+        </div>
+    )
+}
+
+export default Footer;
---
+++ portfolio-v2/src/Header.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+function Header(){
+    return(
+        <div className="header">
+            <h1>MY PORTFOLIO</h1>
+            <button class="button-85" role="button">
+                RESUME</button>
+        </div>
+    )
+}
+
+export default Header;
